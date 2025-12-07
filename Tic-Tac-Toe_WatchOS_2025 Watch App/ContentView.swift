c84d5f9e8da6c86a3d6f1efd78628245138ed808
James Bush <jamesalanbush1972@gmail.com>
2025-12-07 00:16:14 -0500

    Initial Commit

---
+++ Tic-Tac-Toe_WatchOS_2025 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe_WatchOS_2025 Watch App
+//
+//  Created by Xcode Developer on 12/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic-Tac-Toe_WatchOS_2025 Watch App/Tic_Tac_Toe_WatchOS_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_WatchOS_2025App.swift
+//  Tic-Tac-Toe_WatchOS_2025 Watch App
+//
+//  Created by Xcode Developer on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_WatchOS_2025_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
